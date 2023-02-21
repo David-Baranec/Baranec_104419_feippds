eaf89785ee33e86ab7c449a42bcbf48edb29f3a9
David-Baranec <xbaranecd@stuba.sk>
2023-02-21 21:54:51 +0100

    fix!: bakery algorithm works (multiple threads)

--- bakery.py
+++ bakery.py
@@ -7,12 +7,13 @@ Bakery Algorithm is an algorithm that basically works as a generalized solution 
 __author__ = "David Baranec, Tomáš Vavro"
 __email__ = "xbaranecd@stuba.sk"
 __license__ = "MIT"
+__web_sources__ = "https://cppsecrets.com/users/120612197115104981111171149751485164103109971051084699111109/Python-Implementation-of-Bakery-Algorithm.php"
 
 from fei.ppds import Thread
 from time import sleep
 
-tickets: tickets = [0, 1, 2, 3, 4]
-entering: entering = [False] * 5
+tickets: tickets = [0] * 7
+entering: entering = [0] * 7
 
 
 def process(tid: int, num_runs: int):
@@ -27,33 +28,33 @@ def process(tid: int, num_runs: int):
     for _ in range(num_runs):
 
         # process wants to enter critical section
-        entering[tid] = True
+        entering[tid] = 1
 
         maximum = 0
         for ticket in tickets:
             maximum = max(maximum, ticket)
         tickets[tid] = maximum + 1
-        entering[tid] = False
+        entering[tid] = 0
+        sleep(1)
         for i in range(len(tickets)):
 
-                # Wait until thread j receives its number:
-            while entering[i]:
+                # Wait until thread i receives its number:
+            while entering[i]==1:
                  print("waiting on enter")
 
                 # Wait until all threads with smaller numbers or with the samenumber, but with higher priority, finish their work:
             while tickets[i] != 0 and (tickets[tid] > tickets[i] or (tickets[tid] == tickets[i] and tid > i)):
-                 print("waiting")
+                print("")
 
-            ###########################
         # execute critical section
         print(f"Process {tid} runs a complicated computation!")
-        #sleep(1)
-
+        # shows code integrity proof
+        print(f"Process {tid} finished a complicated computation!")
         # exit critical section
         tickets[tid] = 0
 
 if __name__ == '__main__':
-    DEFAULT_NUM_RUNS = 5
+    DEFAULT_NUM_RUNS = 10
     NUM_THREADS = 3
     threads = [Thread(process, i, DEFAULT_NUM_RUNS) for i in range(NUM_THREADS)]
     [t.join() for t in threads]
