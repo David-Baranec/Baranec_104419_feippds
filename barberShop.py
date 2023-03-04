e991249ef1de966c025601fa11fc49ae67ac7704
David-Baranec <xbaranecd@stuba.sk>
2023-03-04 17:17:29 +0100

    docs: description of functions and arguments

--- barberShop.py
+++ barberShop.py
@@ -6,11 +6,9 @@ Faculty: FEI Faculty of Electrical Engineering and Information Technology
 Year: 2023
 """
 
-
 __authors__ = "Dávid Baranec, Marián Šebeňa"
 __email__ = "xbaranecd@stuba.sk, mariansebena@stuba.sk, xvavro@stuba.sk"
 __license__ = "MIT"
-
 
 from fei.ppds import Mutex, Thread, Semaphore
 from time import sleep
@@ -24,10 +22,16 @@ Global arguments:
     C      -- Number of customers
     N      -- Size of waiting room
 """
+
+
 class Shared(object):
+    """Shared class for process alignment
+
+    Arguments:
+        object      -- Shared object for all classes to represent Rendezvous
+    """
 
     def __init__(self):
-
         self.mutex = Mutex()
         self.waiting_room = 0
         self.customer = Semaphore(0)
@@ -37,32 +41,56 @@ class Shared(object):
 
 
 def get_haircut(i):
+    """Simulates the process of sitting at the barbershop.
+
+    Arguments:
+        i     -- Customer identifier
+    """
     print(f"Customer: {i} gets haircut")
-    sleep(randint(3,5))
+    sleep(randint(3, 5))
+
 
 def cut_hair():
+    """Simulates time, when barber cuts hair.
+    """
     print("Barber: cuts hair!")
-    sleep(randint(3,5))
+    sleep(randint(3, 5))
 
 
 def balk(i):
+    """Simulates the time until the customer tries to enter the barbershop again because there was no free space.
+
+    Arguments:
+        i     -- Customer identifier
+    """
     print(f"Customer: {i} waiting room is full!")
-    sleep(randint(5,8))
+    sleep(randint(5, 8))
 
 
 def growing_hair(i):
+    """Simulates the time until the customer tries to enter the barbershop again because there was no free space.
 
-    sleep(randint(8,14))
+    Arguments:
+        i     -- Customer identifier
+    """
+    print(f"Customer: {i} hair is growing!")
+    sleep(randint(8, 14))
 
 
 def customer(i, shared):
+    """The person of the customer who wants to get a haircut.
+
+    Arguments:
+        i      -- Customer identifier
+        shared -- Shared class for synchronization
+    """
     global N
 
     while True:
         shared.mutex.lock()
-        if (shared.waiting_room<N ):
+        if shared.waiting_room < N:
             print(f"Customer: {i} is in the room!")
-            shared.waiting_room+=1
+            shared.waiting_room += 1
             shared.mutex.unlock()
 
             shared.customer.signal()
@@ -84,9 +112,12 @@ def customer(i, shared):
             balk(i)
 
 
-
-
 def barber(shared):
+    """Barber serving customers.
+
+    Arguments:
+        shared -- Shared class for synchronization
+    """
 
     while True:
         shared.customer.wait()
@@ -109,7 +140,6 @@ def main():
 
     for t in customers + [hair_stylist]:
         t.join()
-
 
 
 if __name__ == "__main__":
