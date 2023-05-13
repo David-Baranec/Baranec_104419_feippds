d847f6917ddb9a95185e5e71857380e00dea6212
David-Baranec <xbaranecd@stuba.sk>
2023-05-13 22:29:18 +0200

    classic version using matrix

--- main.c
+++ main.c
@@ -1,178 +1,141 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#include <math.h>
-#include <time.h>
 
-#define DAMPING_FACTOR 0.85
-#define TOLERANCE 0.0001
+#define N 30
+#define D 0.85
 
-int cmpfunc(const void *a, const void *b) {
-    const double *da = *(const double **)a;
-    const double *db = *(const double **)b;
-    return (*db > *da) - (*db < *da);
+int* get_counts_send(int processes_count, int vertices_count) {
+    int* sendcounts = (int*)malloc(processes_count * sizeof(int));
+    int i;
+    double average = ((double) vertices_count) / ((double) processes_count);
+    double last = 0.0;
+    for (i=0; i < processes_count; i++) {
+        sendcounts[i] = ((int) (last + average)) - ((int) last);
+        last += average;
+    }
+    return sendcounts;
 }
 
-void sort_indexes_desc(double *arr, int *idx, int n) {
-    double **p = malloc(n * sizeof(double *));
-    for (int i = 0; i < n; i++) {
-        p[i] = &arr[i];
-        idx[i] = i;
+int* get_displacements(int processes_count, const int* counts_send) {
+    int* displs = (int*)malloc(processes_count * sizeof(int));
+    int i;
+    int j = 0;
+    for (i=0; i < processes_count; i++) {
+        displs[i] = j;
+        j += counts_send[i];
     }
-    qsort(p, n, sizeof(double *), cmpfunc);
-    for (int i = 0; i < n; i++) {
-        idx[i] = p[i] - arr;
-    }
-    free(p);
+    return displs;
 }
 
-int main(int argc, char** argv) {
-    double start_time, end_time, elapsed_time;
-    clock_t start_time_total, end_time_total;
-    start_time_total = clock();
-    double total_time;
-    int my_rank, comm_size;
-    int i, j;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
-    start_time = MPI_Wtime();
-    int n;
-
-    char row[1000];
-    int count;
-    int my_sum = 0;
-
-    // Open the input file for reading
-    FILE *input_file = fopen("input.txt", "r");
-    if (input_file == NULL) {
-        fprintf(stderr, "Error: could not open input file\n");
-        exit(1);
+void loadMatrixFromFile(const char* filename, int matrix[N][N]) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return;
     }
 
-    // Read in the number of vertices
-    fscanf(input_file, "%d", &n);
-
-    // Allocate memory for the follower counts array
-    int *follower_counts = (int *) malloc(n * sizeof(int));
-    if (follower_counts == NULL) {
-        fprintf(stderr, "Error: could not allocate memory\n");
-        exit(1);
-    }
-
-    // Read in the follower data from the input file and store it in the follower counts array
-    for (i = 0; i < n; i++) {
-        fscanf(input_file, "%s", row);
-        count = 0;
-        for (j = 0; j < n; j++) {
-            if (row[j] == '1') {
-                count++;
-            }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            fscanf(file, "%1d", &matrix[i][j]);
         }
-        follower_counts[i] = count;
     }
 
-    // Close the input file
-    fclose(input_file);
+    fclose(file);
 
-    // Compute the sum of follower counts across all nodes
-    for (i = my_rank; i < n; i += comm_size) {
-        my_sum += follower_counts[i];
+}
+int main(int argc, char** argv) {
+    // Inicializácia MPI enviromentu
+    MPI_Init(&argc, &argv);
+    int process_id, processes_count;
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+    MPI_Comm_size(MPI_COMM_WORLD, &processes_count);
+    double* ranks = NULL;
+    int** graph_matrix = NULL;
+    int* partitions_counts = NULL;
+    int* partitions_indexes = NULL;
+    int vertices_count = 0;
+    int iterations_count = 0;
+    int f, g;
+
+    int matrix[N][N];
+    loadMatrixFromFile("input.txt", matrix);
+    iterations_count = 100;
+    vertices_count = N;
+    graph_matrix = (int**)malloc(N * sizeof(int*));
+    for (f = 0; f < N; f++)
+        graph_matrix[f] = (int*)malloc(N * sizeof(int));
+    for (f = 0; f < N; f++)
+        for (g = 0; g < N; g++)
+            graph_matrix[f][g] = matrix[f][g];
+
+
+    // Inicilizácia pola rank-ov
+    ranks = (double*) malloc(vertices_count * sizeof(double));
+    int m;
+    for (m = 0; m < vertices_count; m++) {
+        ranks[m] = 1.0 / vertices_count;
     }
 
-    // Compute the total sum across all nodes using MPI_Reduce
-    int total_sum = 0;
-    MPI_Reduce(&my_sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // Partitioning dát
+    partitions_counts = get_counts_send(processes_count, vertices_count);
+    partitions_indexes = get_displacements(processes_count, partitions_counts);
+    int partition_vertices_count = partitions_counts[process_id];
+    int partition_vertex_index = partitions_indexes[process_id];
+    double* partition_ranks = (double*)malloc(partition_vertices_count * sizeof(double));
+    double* tmp_ranks = (double*)malloc(partition_vertices_count * sizeof(double));
+    double tmp_out_links_count;
+    int i, j, k, l;
 
-    // Broadcast the total sum to all nodes
-    MPI_Bcast(&total_sum, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // PageRank algoritmus - iterácie
+    for (k = 0; k < iterations_count; k++) {
 
-    // Normalize the follower counts to get the initial Page Rank scores
-    double *page_rank = (double *) malloc(n * sizeof(double));
-    double init_score = 1.0 / n;
-    for (i = 0; i < n; i++) {
-        page_rank[i] = init_score * follower_counts[i];
+        // MPI Scatter - distribúcia (odoslanie) dát (rank-ov) procesom
+        MPI_Scatterv(ranks, partitions_counts, partitions_indexes, MPI_DOUBLE,
+                     partition_ranks, partition_vertices_count, MPI_DOUBLE,
+                     0, MPI_COMM_WORLD);
+
+        // PageRank algoritmus - výpočet
+        for (i = partition_vertex_index; i < partition_vertex_index + partition_vertices_count; i++) {
+            tmp_ranks[i - partition_vertex_index] = 0.0;
+            for (j = 0; j < vertices_count; j++) {
+                if (graph_matrix[j][i] == 1) {
+                    tmp_out_links_count = 0.0;
+                    for (l = 0; l < vertices_count; l++) {
+                        tmp_out_links_count += graph_matrix[j][l];
+                    }
+                    tmp_ranks[i - partition_vertex_index] += ranks[j] / tmp_out_links_count;
+                }
+            }
+            partition_ranks[i - partition_vertex_index] = (1.0 - D) + D * tmp_ranks[i - partition_vertex_index];
+        }
+
+        // MPI Allgater - distribúcia (prijatie) dát (rank-ov) procesmi
+        MPI_Allgatherv(partition_ranks, partition_vertices_count, MPI_DOUBLE,
+                       ranks, partitions_counts, partitions_indexes, MPI_DOUBLE,
+                       MPI_COMM_WORLD);
+
+    }
+    free(partitions_counts);
+    free(partitions_indexes);
+    free(partition_ranks);
+    free(tmp_ranks);
+
+
+    if (process_id == 0) {
+        // Print the final PageRank scores
+        for (f = 0; f < N; f++) {
+            printf("\nPage %d: %lf\n", f + 1, ranks[f]);
+        }
     }
 
-    // Iterate
+    for (f = 0; f < N; f++)
+        free(graph_matrix[f]);
+    free(graph_matrix);
+    free(ranks);
 
-    int num_iterations = 0;
-double diff = TOLERANCE + 1;
-while (diff > TOLERANCE) {
-// Compute the contribution from incoming edges
-double *incoming_scores = (double *) calloc(n, sizeof(double));
-for (i = my_rank; i < n; i += comm_size) {
-for (j = 0; j < n; j++) {
-if (j != i && follower_counts[j] > 0) {
-incoming_scores[i] += page_rank[j] / follower_counts[j];
-}
-}
-incoming_scores[i] *= DAMPING_FACTOR;
-}
+    // De-inicializácia MPI enviromentu
+    MPI_Finalize();
 
-// Compute the new Page Rank scores
-double *new_page_rank = (double *) malloc(n * sizeof(double));
-double sum = 0.0;
-for (i = my_rank; i < n; i += comm_size) {
-new_page_rank[i] = incoming_scores[i] + (1 - DAMPING_FACTOR) * init_score;
-sum += new_page_rank[i];
-}
-
-// Compute the sum of new Page Rank scores across all nodes using MPI_Allreduce
-double total_new_score = 0.0;
-MPI_Allreduce(&sum, &total_new_score, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-
-// Compute the difference between the old and new scores
-diff = 0.0;
-for (i = my_rank; i < n; i += comm_size) {
-new_page_rank[i] /= total_new_score;
-diff += fabs(new_page_rank[i] - page_rank[i]);
-}
-
-// Compute the sum of differences across all nodes using MPI_Reduce
-double total_diff = 0.0;
-MPI_Reduce(&diff, &total_diff, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-// Update the Page Rank scores using MPI_Allgather to gather scores from all nodes
-MPI_Allgather(&new_page_rank[my_rank], n / comm_size, MPI_DOUBLE, page_rank, n / comm_size, MPI_DOUBLE, MPI_COMM_WORLD);
-
-num_iterations++;
-free(incoming_scores);
-free(new_page_rank);
-}
-
-double fin = 0.0;
-// Print the final Page Rank scores on the root process
-if (my_rank == 0) {
-printf("Page Rank scores:\n");
-for (i = 0; i < n; i++) {
-printf("%f ", page_rank[i]);
-fin += page_rank[i];
-}
-printf("\n");
-printf("Sum in pageRank %f\n", fin);
-
-int idx[n];
-sort_indexes_desc(page_rank, idx, n);
-
-printf("\nIndex order in descending order based on value: ");
-for (int i = 0; i < n; i++) {
-printf("%d ", idx[i]);
-}
-printf("\n");
-}
-
-free(follower_counts);
-free(page_rank);
-end_time = MPI_Wtime();
-elapsed_time = end_time - start_time;
-
-printf("\nRank %d: Time taken = %f seconds", my_rank, elapsed_time);
-end_time_total = clock(); // Record the ending time
-total_time = (double)(end_time_total - start_time_total) / CLOCKS_PER_SEC;
-printf("\nTotal Time taken = %f seconds", total_time);
-
-MPI_Finalize();
-
-return 0;
+    return 0;
 }
